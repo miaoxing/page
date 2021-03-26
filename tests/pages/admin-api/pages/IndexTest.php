02239a0bf810fc93be6ee18db813272108a828c5
twinh <twinhuang@qq.com>
2021-03-26 22:08:30 +0800

    test: 增加后端列表页测试

---
+++ tests/pages/admin-api/pages/IndexTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace MiaoxingTest\Page\Pages\AdminApi\Pages;
+
+use Miaoxing\Page\Service\PageModel;
+use Miaoxing\Plugin\Service\Tester;
+use Miaoxing\Plugin\Service\User;
+use Miaoxing\Plugin\Test\BaseTestCase;
+
+class IndexTest extends BaseTestCase
+{
+    public function testGet()
+    {
+        User::loginById(1);
+
+        $page = PageModel::save([
+            'name' => '测试页面',
+        ]);
+
+        $ret = Tester::request(['limit' => 1])->getAdminApi('pages');
+
+        $this->assertSame($page->toArray(), $ret['data'][0], 'order by id');
+    }
+}

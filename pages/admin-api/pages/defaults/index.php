f3ec5d0163c8029173feb652326ddb6295496cd6
twinh <twinhuang@qq.com>
2022-04-12 10:19:23 +0800

    refactor: 使用 `DefaultsTrait` 简化默认值页面

--- pages/admin-api/pages/defaults/index.php
+++ pages/admin-api/pages/defaults/index.php
@@ -1,11 +1,8 @@
 <?php
 
-use Miaoxing\Page\Service\PageModel;
 use Miaoxing\Plugin\BaseController;
+use Miaoxing\Services\Page\DefaultsTrait;
 
 return new class () extends BaseController {
-    public function get()
-    {
-        return PageModel::toRet();
-    }
+    use DefaultsTrait;
 };
